ff7dc39eacf16b692214d37e75e2c9119106ab45
DESKTOP-E7JOVBC\FirstMan <pb891215@gmail.com>
2022-08-24 17:08:31 +0900

    Fix: Update create e2e test

--- cypress/integration/create.js
+++ cypress/integration/create.js
@@ -60,6 +60,7 @@ describe('Create Page', () => {
                 value: 'foo',
             },
         ]);
+        CreatePage.submit();
         cy.get('.ra-input-backlinks').contains('Required');
     });
 
@@ -90,7 +91,7 @@ describe('Create Page', () => {
                 value: 'Annamarie Mayer',
             },
         ]);
-        cy.get('[role="option"]').trigger('click');
+        cy.get('[role="option"]:first').trigger('click');
         cy.get(CreatePage.elements.input('authors.0.role')).should(
             el => expect(el).to.exist
         );
